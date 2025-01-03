b41d0cf7d3fdb3be9650e27dbc21cf80e6b3763e
Arcodiant <andrew.ch.armstrong@gmail.com>
2025-01-03 17:27:23 -0500

    Improve cookie consent checks

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ def define_env(env):
 
     @env.macro
     def version_check(version):
-        if packaging.version.parse(version) < packaging.version.parse("3.24.3"):
+        if packaging.version.parse(version) < packaging.version.parse("4.0.0"):
             log.warning("Doc file '"+env.page.file.src_uri+"' failed version check with " + version)
             return "!!! danger inline end \"Last Updated\"\n    Alpha "+version+"\n\n    Information on this page may be outdated."
         else:
-            return "!!! success inline end \"Last Updated\"\n    [Alpha "+version+"](https://robertsspaceindustries.com/comm-link/Patch-Notes/20309-Star-Citizen-Alpha-3243)"
+            return "!!! success inline end \"Last Updated\"\n    [Alpha "+version+" Preview](https://robertsspaceindustries.com/comm-link/Patch-Notes/20360-Star-Citizen-Alpha-40)"
